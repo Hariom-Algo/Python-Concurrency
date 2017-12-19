6e9c60065b719369aab5ffc6b7239bc57bfd32eb
hellowindia8087@gmail.com <hellowindia8087@gmail.com>
2017-12-19 14:26:54 +0530

    Added two threads

--- Threading.py
+++ Threading.py
@@ -6,7 +6,15 @@ def threadWorker():
     time.sleep(10)
     print("My work is done")
 
+def threadWorker1():
+    print("My thread 1 has entered the Running state ")
+    time.sleep(5)
+    print("My thread 1 work is done")
+
 myThread = threading.Thread(target=threadWorker)
+myThread1 = threading.Thread(target=threadWorker1)
 myThread.start()
+myThread1.start()
 myThread.join()
+myThread1.join()
 print("Thread Entered the dead state")
